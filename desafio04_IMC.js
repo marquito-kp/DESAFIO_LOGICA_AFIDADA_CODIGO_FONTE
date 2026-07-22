c904f4ba1b80ad068d26442d51d26d3e64786ea7
Marcos Krol Pacheco <marcos.krolp@gmail.com>
2026-07-22 10:19:08 -0300

    Up do primeiro desafio em código do dia 02.

---
+++ desafio04_IMC.js
@@ -0,0 +1,11 @@
+// DESAFIO 04 - CÁLCULO DO IMC - Primeiro desafio em código
+// Criar um código para cálculo o meu IMC (Indice de Massa Corporal)
+
+// Variáveis de peso e altura
+let peso= 140; // kilogramas
+let altura= 1.89; // metros
+
+let imc = peso/(altura**2); // cálculo do IMC
+
+// Demonstra no terminal o resultado
+console.log('O seu IMC é igual a '+ imc);
